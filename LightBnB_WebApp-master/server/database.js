9a2621a80ea43ef7878c19bf8b879cd764b9ff5c
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-11-26 03:06:23 +0000

    add queries to allProperties function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -80,7 +80,6 @@ const getAllReservations = function(guest_id, limit = 10) {
     FROM properties
     JOIN reservations ON properties.id = property_id
     WHERE guest_id = $1 
-    AND start_date > NOW()::date
     LIMIT $2;`, [guest_id,limit])
   
     .then((result) => {
@@ -102,13 +101,46 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
  const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+  const queryParams = [];
+    let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id
+    `;
+  
+    if (options.city) {
+      queryParams.push(`%${options.city}%`);
+      queryString += `WHERE city LIKE $${queryParams.length} `;
+    }
+
+    if (options.owner_id) {
+      queryParams.push(options.owner_id);
+      queryString += `AND properties.owner_id LIKE $${queryParams.length} `;
+    }
+
+    if (options.minimum_price_per_night && options.maximum_price_per_night) {
+      queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100);
+      queryString += `AND (properties.cost_per_night >= $${queryParams.length - 1} AND properties.cost_per_night <= $${queryParams.length})`;
+    }
+    queryParams.push(limit);
+    queryString += `
+    GROUP BY properties.id`
+
+   if (options.minimum_rating) {
+     queryParams.push(options.minimum_rating);
+     queryString += `AND property_reviews.rating >= $${queryParams.length}`;
+   }
+   
+    queryParams.push(limit);
+    queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+  
+    return pool.query(queryString, queryParams)
     .then((result) => result.rows)
-    .catch((err) => {
-      console.log(err.message);
-    });
-};
+    .catch((err) => console.log(err.message));
+  };
 exports.getAllProperties = getAllProperties;
 
 /**
